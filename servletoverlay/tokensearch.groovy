6339db5706444aacbd10ebdeb138fff971211696
Neel Smith <dnsmith.neel@gmail.com>
2014-04-17 14:23:18 -0400

    Add simple search by literal word.

--- servletoverlay/tokensearch.groovy
+++ servletoverlay/tokensearch.groovy
@@ -36,18 +36,17 @@ String getSparqlReply(String acceptType, String query) {
     return replyString
 }
 
-String getQuery(String lemma) {
+String getQuery(String form) {
 String reply = """
-select ?lex ?form ?lemma ?formstr ?psg where {
-?lex <http://www.homermultitext.org/hmt/citedata/latlexent_Lemma> "${lemma}" .
+select ?lex ?form ?lemma ?psg where {
+?form <http://www.homermultitext.org/hmt/citedata/tokens_String> "${form}" .
 ?lex <http://www.w3.org/1999/02/22-rdf-syntax-ns#label>  ?lemma .
 ?lex  <http://shot.holycross.edu/rdf/hclat/hasForm> ?form .
 ?form   <http://www.homermultitext.org/cite/rdf/occursIn>  ?psg .
-?form <http://www.homermultitext.org/hmt/citedata/tokens_String> ?formstr .
-}
-ORDER BY ?lex ?formstr 
-"""
 
+}
+ORDER BY ?lex 
+"""
 return reply
 }
 
@@ -57,16 +56,13 @@ def slurper = new groovy.json.JsonSlurper()
 
 
 // ADD ERROR CHECK ON PARAMS...
-String queryString = getQuery(params.lemma)
+String queryString = getQuery(params.word)
 def parsedReply = slurper.parseText(getSparqlReply("application/json", queryString))
-
-def firstReply = parsedReply.results.bindings[0]
-String lemmaStr = firstReply.lemma.value
 
 
 html.html {
     head {
-      title("Morphological search: ${lemmaStr}")
+      title("Word search: ${params.word}")
         link(type : "text/css", rel : "stylesheet", href : "css/normalize.css", title : "CSS stylesheet")
         link(type : "text/css", rel : "stylesheet", href : "css/latin.css", title : "CSS stylesheet")
     }
@@ -76,26 +72,30 @@ html.html {
             nav (role : "navigation") {
                 a(href : '@homeUrl@', "Home")
             }
-            h1('Morphological search matching lemma form "' + lemmaStr + '"')
+            h1('Search for literal form "' + params.word + '"')
     	}
     	
     	article {
+	  mkp.yield "Num results: ${parsedReply.results.bindings.size()}"
 	  ul {
 	    parsedReply.results.bindings.each { b ->
 	      try {
 		CtsUrn urn = new CtsUrn(URLDecoder.decode(b.psg.value))
 		CiteUrn lexUrn = new CiteUrn(b.lex.value)
 		li {
+		  mkp.yield "${urn} -> ${lexUrn}"
 		  String nodeStr = urn.getPassageNode()
 		  String psgUrn = urn.getUrnWithoutPassage() + ":" + nodeStr
-		  strong(b.formstr.value)
-		  mkp.yield " (lexical entity ${lexUrn.getObjectId()}): occurs in "
-		  String linkStr = "line ${urn.getPassageNode()} of ${urn.getUrnWithoutPassage()}. "
-		  a(href:"facsimile.groovy?urn=${psgUrn}", linkStr)
+		  mkp.yield " (${lexUrn.getObjectId()}): occurs in "
+	  String linkStr = "line ${urn.getPassageNode()} of ${urn.getUrnWithoutPassage()}. "
+		  		  a(href:"facsimile.groovy?urn=${psgUrn}", linkStr)
 		  mkp.yield "."
 		}
 
 	      } catch (Exception e) {
+		System.err.println "Something went wrong."
+		System.err.println "Tried CTS URN " + b.psg.value + " and CITE lex ent " + b.lex.value
+		
 	      }
 	    }
 	  }
